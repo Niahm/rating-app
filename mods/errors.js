c758ffa0f7b40d296d512ad0302a6d28670c107d
maxbbn <qipbbn@gmail.com>
2012-10-17 18:28:05 +0800

    fix

--- mods/errors.js
+++ mods/errors.js
@@ -22,8 +22,7 @@ var NoPermission = exports.NoPermission =  function(msg){
 util.inherits(NoPermission, Error);
 NoPermission.prototype.status = 401;
 
-exports.errorHandle = function(err, req, res, next){
-    console.dir(err);
+exports.errorHandle = function (err, req, res, next) {
     if(err instanceof NotFound){
         res.render('404',{
             title : 404
